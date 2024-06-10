22da1d6c0922707538b51ab8de80444ddd81eed6
Vladimir Polozov <z2005q.polozov@yandex.ru>
2024-06-10 17:20:52 +0700

    add database and form

--- WarehouseSystem/WarehouseSystem/Form1.Designer.cs
+++ WarehouseSystem/WarehouseSystem/Form1.Designer.cs
@@ -29,12 +29,151 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.ProductsLabel = new System.Windows.Forms.Label();
+            this.WarehouseSystemLabel = new System.Windows.Forms.Label();
+            this.AddNewProductButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.weightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.thresholdValueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.warehouseSystemDataSet = new WarehouseSystem.WarehouseSystemDataSet();
+            this.productsTableAdapter = new WarehouseSystem.WarehouseSystemDataSetTableAdapters.ProductsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseSystemDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ProductsLabel
+            // 
+            this.ProductsLabel.AutoSize = true;
+            this.ProductsLabel.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ProductsLabel.Location = new System.Drawing.Point(12, 12);
+            this.ProductsLabel.Name = "ProductsLabel";
+            this.ProductsLabel.Size = new System.Drawing.Size(72, 22);
+            this.ProductsLabel.TabIndex = 0;
+            this.ProductsLabel.Text = "Товары";
+            this.ProductsLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // WarehouseSystemLabel
+            // 
+            this.WarehouseSystemLabel.AutoSize = true;
+            this.WarehouseSystemLabel.Font = new System.Drawing.Font("Montserrat Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.WarehouseSystemLabel.Location = new System.Drawing.Point(12, 386);
+            this.WarehouseSystemLabel.Name = "WarehouseSystemLabel";
+            this.WarehouseSystemLabel.Size = new System.Drawing.Size(74, 22);
+            this.WarehouseSystemLabel.TabIndex = 1;
+            this.WarehouseSystemLabel.Text = "Склады";
+            this.WarehouseSystemLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // AddNewProductButton
+            // 
+            this.AddNewProductButton.Font = new System.Drawing.Font("Montserrat Medium", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddNewProductButton.Location = new System.Drawing.Point(463, 12);
+            this.AddNewProductButton.Name = "AddNewProductButton";
+            this.AddNewProductButton.Size = new System.Drawing.Size(96, 29);
+            this.AddNewProductButton.TabIndex = 2;
+            this.AddNewProductButton.Text = "Добавить";
+            this.AddNewProductButton.UseVisualStyleBackColor = true;
+            this.AddNewProductButton.Click += new System.EventHandler(this.AddNewProductButton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.weightDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.thresholdValueDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.productsBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(543, 200);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // weightDataGridViewTextBoxColumn
+            // 
+            this.weightDataGridViewTextBoxColumn.DataPropertyName = "Weight";
+            this.weightDataGridViewTextBoxColumn.HeaderText = "Weight";
+            this.weightDataGridViewTextBoxColumn.Name = "weightDataGridViewTextBoxColumn";
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // thresholdValueDataGridViewTextBoxColumn
+            // 
+            this.thresholdValueDataGridViewTextBoxColumn.DataPropertyName = "ThresholdValue";
+            this.thresholdValueDataGridViewTextBoxColumn.HeaderText = "ThresholdValue";
+            this.thresholdValueDataGridViewTextBoxColumn.Name = "thresholdValueDataGridViewTextBoxColumn";
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataMember = "Products";
+            this.productsBindingSource.DataSource = this.warehouseSystemDataSet;
+            // 
+            // warehouseSystemDataSet
+            // 
+            this.warehouseSystemDataSet.DataSetName = "WarehouseSystemDataSet";
+            this.warehouseSystemDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productsTableAdapter
+            // 
+            this.productsTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1043, 715);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.AddNewProductButton);
+            this.Controls.Add(this.WarehouseSystemLabel);
+            this.Controls.Add(this.ProductsLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.warehouseSystemDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label ProductsLabel;
+        private System.Windows.Forms.Label WarehouseSystemLabel;
+        private System.Windows.Forms.Button AddNewProductButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private WarehouseSystemDataSet warehouseSystemDataSet;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private WarehouseSystemDataSetTableAdapters.ProductsTableAdapter productsTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn weightDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn thresholdValueDataGridViewTextBoxColumn;
     }
 }
 
--- WarehouseSystem/WarehouseSystem/Form1.cs
+++ WarehouseSystem/WarehouseSystem/Form1.cs
@@ -1,20 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WarehouseSystem
 {
     public partial class Form1 : Form
     {
+        public SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Надежда\\source\\repos\\IndividualProject\\WarehouseSystem\\WarehouseSystem\\WarehouseSystem.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+        public SqlDataAdapter adapter;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void AddNewProductButton_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+             
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                string query = "insert into ()" +
+                " values('')";
+
+                SqlCommand command = new SqlCommand(query, connection);
+                command.ExecuteNonQuery();
+                MessageBox.Show("Connection was successful!");
+            }
+
+            connection.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
